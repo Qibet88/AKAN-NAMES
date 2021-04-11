61f727793716482990c90307ad1d1835dc12153d
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-04-11 11:25:32 +0300

    More code on gender

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function valdate(){
+/*function valdate(){
     var dateOfBirth = document.getElementById("date").value;
     if (dateOfBirth < 1 || dateOfBirth > 31);{
     alert("Enter a valid date");
@@ -9,70 +9,73 @@ function valmonth(){
     if (monthOfBirth < 1 || monthOfBirth > 12);{
     alert("Enter a valid month")
     }
-}
+}*/
 function dayOfWeek(){
-    var cc = parseInt(prompt("Please enter Century digit"));
-    var yy = parseInt(prompt("Please enter year digit"));
-    var mm = parseInt(prompt("Please Enter Month"));
-    var dd = parseInt(prompt("Enter Day of month"));
+    var yearOfBirth = document.getElementById("year").value;
+    var cc = yearOfBirth.slice(0,2);
+    var yy = yearOfBirth.slice(2,4);
+    var mm = parseInt(document.getElementById("month").value);
+    var dd = parseInt(document.getElementById("date").value);
     return ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd )%7;
 }
 document.getElementById("month").innerHTML = dayOfWeek();
-var x = dayOfWeek().value;
+var result = Math.floor(dayOfWeek().value);
+var daysOfWeek = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"]
 var akanMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var akanFemale = ["Akosua","Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-if (Option.value == "male" && dayOfWeek == 0){
-    document.getElementById("demo").innerHTML = "Your akan name is " + akanMale[0];
+if (Option.value == "male" && result == 0){
+    document.getElementById("akan").innerHTML = "Your akan name is " + akanMale[0];
 }
-    else if(Option.value == "male" && dayOfWeek == 1){
-            document.getElementById("demo").innerHTML = "Your akan name is " + akanMale[1];
+    else if(Option.value == "male" && result == 1){
+            document.getElementById("akan").innerHTML = "Your akan name is " + akanMale[1];
 
     }
-    else if(Option.value == "male" && dayOfWeek == 2){
-        document.getElementById("demo").innerHTML = "Your akan name is " + akanMale[2];
+    else if(Option.value == "male" && result == 2){
+        document.getElementById("akan").innerHTML = "Your akan name is " + akanMale[2];
 
 }
-else if(Option.value == "male" && dayOfWeek == 3){
-    document.getElementById("demo").innerHTML = "Your akan name is " + akanMale[3];
+else if(Option.value == "male" && result == 3){
+    document.getElementById("akan").innerHTML = "Your akan name is " + akanMale[3];
 
 }
-else if(Option.value == "male" && dayOfWeek == 4){
-    document.getElementById("demo").innerHTML = "Your akan name is " + akanMale[4];
+else if(Option.value == "male" && result == 4){
+    document.getElementById("akan").innerHTML = "Your akan name is " + akanMale[4];
 
 }
-else if(Option.value == "male" && dayOfWeek == 5){
-    document.getElementById("demo").innerHTML = "Your akan name is " + akanMale[5];
+else if(Option.value == "male" && result == 5){
+    document.getElementById("akan").innerHTML = "Your akan name is " + akanMale[5];
 
 }
-else (Option.value == "male" && dayOfWeek == 6){
-    document.getElementById("demo").innerHTML = "Your akan name is " + akanMale[6];
+else (Option.value == "male" && result == 6){
+    document.getElementById("akan").innerHTML = "Your akan name is " + akanMale[6];
 
 }
-if (Option.value == "female" && dayOfWeek == 0){
-    document.getElementById("demo").innerHTML = "Your akan name is " + akanFemale[0];
+
+if (Option.value == "female" && result == 0){
+    document.getElementById("akan").innerHTML = "Your akan name is " + akanFemale[0];
 }
-    else if(Option.value == "female" && dayOfWeek == 1){
-            document.getElementById("demo").innerHTML = "Your akan name is " + akanFemale[1];
+    else if(Option.value == "female" && result == 1){
+            document.getElementById("akan").innerHTML = "Your akan name is " + akanFemale[1];
 
     }
-    else if(Option.value == "female" && dayOfWeek == 2){
-        document.getElementById("demo").innerHTML = "Your akan name is " + akanFemale[2];
+    else if(Option.value == "female" && result == 2){
+        document.getElementById("akan").innerHTML = "Your akan name is " + akanFemale[2];
 
 }
-else if(Option.value == "female" && dayOfWeek == 3){
-    document.getElementById("demo").innerHTML = "Your akan name is " + akanFemale[3];
+else if(Option.value == "female" && result == 3){
+    document.getElementById("akan").innerHTML = "Your akan name is " + akanFemale[3];
 
 }
-else if(Option.value == "female" && dayOfWeek == 4){
-    document.getElementById("demo").innerHTML = "Your akan name is " + akanFemale[4];
+else if(Option.value == "female" && result == 4){
+    document.getElementById("akan").innerHTML = "Your akan name is " + akanFemale[4];
 
 }
-else if(Option.value == "female" && dayOfWeek == 5){
-    document.getElementById("demo").innerHTML = "Your akan name is " + akanFemale[5];
+else if(Option.value == "female" && result == 5){
+    document.getElementById("akan").innerHTML = "Your akan name is " + akanFemale[5];
 
 }
-else (Option.value == "male" && dayOfWeek == 6){
-    document.getElementById("demo").innerHTML = "Your akan name is " + akanFemale[6];
+else (Option.value == "male" && result == 6){
+    document.getElementById("akan").innerHTML = "Your akan name is " + akanFemale[6];
 
 }
 
